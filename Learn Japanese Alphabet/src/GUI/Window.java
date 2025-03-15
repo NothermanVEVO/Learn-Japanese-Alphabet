254739a762d6d39af1595d9bd7c3fac8c142d087
Notherman <134525115+NothermanVEVO@users.noreply.github.com>
2025-03-15 16:39:19 -0300

    V 1.1.1
    Removed unused imports

--- Learn Japanese Alphabet/src/GUI/Window.java
+++ Learn Japanese Alphabet/src/GUI/Window.java
@@ -5,9 +5,7 @@ import java.util.List;
 import java.awt.Cursor;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.io.File;
 
-import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 
 import src.Alphabet.Alphabet.Alphabets;
